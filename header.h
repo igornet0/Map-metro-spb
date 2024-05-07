d4179fb38d47f35bc0fbbc73991c18ba15bb7c87
Игорь Михайлец <892130836@mail.ru>
2024-05-07 19:36:21 +0700

    Changes files and bug fixes

---
+++ header.h
@@ -0,0 +1,56 @@
+#include <gtk/gtk.h>
+
+typedef struct _Edge Edge;
+typedef struct _Transition Transition;
+
+typedef struct _Vertex {
+    int index;
+    char *name;
+    int time_on;
+    int time_exit;
+    GdkRGBA color;
+    int x, y; // Координаты для отрисовки
+    int size; // Размер
+    Edge **edges; // Список рёбер
+    Transition **transitions; // Список переходов
+    int num_edges; // Количество рёбер
+    int num_transitions; // Количество переходов
+} Vertex;
+
+typedef struct _Edge {
+    Vertex *from;
+    Vertex *to;
+    int time;
+} Edge;
+
+typedef struct _Transition {
+    Vertex *from;
+    Vertex *to;
+    int time;
+} Transition;
+
+Vertex *create_vertex(int index, char *name, GdkRGBA color, int x, int y, int time_on, int time_exit);
+
+void add_edge(Vertex *v1, Vertex *v2, int time);
+
+void findFastestPath(Vertex *current, Vertex *destination, int currentTime, int *minTime, int *visited, GList **path, GList **min_path);
+
+void find_path(Vertex *start, Vertex *end);
+
+static gboolean draw_callback(GtkWidget *widget, cairo_t *cr, gpointer data);
+
+static gboolean on_key_press(GtkWidget *widget, GdkEventKey *event, gpointer user_data);
+
+static gboolean on_mouse_press(GtkWidget *widget, GdkEventButton *event, gpointer data);
+
+static gboolean on_key_release(GtkWidget *widget, GdkEventKey *event, gpointer user_data);
+
+static gboolean on_mouse_release(GtkWidget *widget, GdkEventButton *event, gpointer user_data);
+
+static gboolean on_motion(GtkWidget *widget, GdkEventMotion *event, gpointer user_data);
+
+static gboolean on_scroll(GtkWidget *widget, GdkEventScroll *event, gpointer user_data);
+
+static gboolean on_draw(GtkWidget *widget, cairo_t *cr, gpointer user_data);
+
+static gboolean on_delete_event(GtkWidget *widget, GdkEvent *event, gpointer user_data);
---
+++ main.c
@@ -0,0 +1,401 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sqlite3.h>
+#include <math.h>
+#include <limits.h>
+#include <stdbool.h>
+#include <cairo.h>
+#include <gtk/gtk.h>
+#include "header.h"
+
+#define MAX_INT 2147483647
+
+typedef struct _Edge Edge;
+typedef struct _Transition Transition;
+typedef struct _Vertex Vertex;
+
+Vertex *selected_vertex_start = NULL;
+Vertex *selected_vertex_end = NULL;
+
+void findFastestPath(Vertex *current, Vertex *destination, int currentTime, int *minTime, int *visited, GList **path, GList **min_path) {
+   visited[current->index] = 1;
+   *path = g_list_append(*path, current);
+
+   if (current == destination) {
+        if (currentTime < *minTime) {
+            *minTime = currentTime;
+            if (*min_path) {
+                g_list_free(*min_path);
+            }
+            *min_path = g_list_copy(*path);
+        }
+    } else {
+        // Перебор всех переходов из текущей вершины
+        for (int i = 0; i < current->num_transitions; i++) {
+            Transition *transition = current->transitions[i];
+            if (!visited[transition->to->index]) {
+                findFastestPath(transition->to, destination, currentTime + transition->time, minTime, visited, path, min_path);
+            }
+        }
+        // Перебор всех ребер из текущей вершины
+        for (int i = 0; i < current->num_edges; i++) {
+            Edge *edge = current->edges[i];
+            if (!visited[edge->to->index]) {
+                findFastestPath(edge->to, destination, currentTime + edge->time, minTime, visited, path, min_path);
+            }
+        }
+    }
+
+
+    visited[current->index] = 0;
+    *path = g_list_remove(*path, current);
+}
+
+Vertex *create_vertex(int index, char *name, GdkRGBA color, int x, int y, int time_on, int time_exit) {
+    Vertex *v = malloc(sizeof(Vertex));
+    v->index = index;
+    v->name = name;
+    v->time_on = time_on;
+    v->time_exit = time_exit;
+    v->color = color;
+    v->x = x;
+    v->y = y;
+    v->size = 10;
+    v->edges = NULL;
+    v->transitions = NULL;
+    v->num_edges = 0;
+    v->num_transitions = 0;
+    return v;
+}
+
+void add_edge(Vertex *v1, Vertex *v2, int time) {
+    Edge *edge = malloc(sizeof(Edge));
+    edge->from = v1;
+    edge->to = v2;
+    edge->time = time;
+
+    v1->edges = realloc(v1->edges, (v1->num_edges + 1) * sizeof(Edge *));
+    v1->edges[v1->num_edges++] = edge;
+
+    Edge *reverse_edge = malloc(sizeof(Edge));
+    reverse_edge->from = v2;
+    reverse_edge->to = v1;
+    reverse_edge->time = time;
+
+    v2->edges = realloc(v2->edges, (v2->num_edges + 1) * sizeof(Edge *));
+    v2->edges[v2->num_edges++] = reverse_edge;
+}
+
+void add_transition(Vertex *v1, Vertex *v2, int time) {
+    Transition *transition = malloc(sizeof(Transition));
+    transition->from = v1;
+    transition->to = v2;
+    transition->time = time;
+
+    v1->transitions = realloc(v1->transitions, (v1->num_transitions + 1) * sizeof(Transition *));
+    v1->transitions[v1->num_transitions++] = transition;
+
+    Transition *reverse_transition = malloc(sizeof(Transition));
+    reverse_transition->from = v2;
+    reverse_transition->to = v1;
+    reverse_transition->time = time;
+
+    v2->transitions = realloc(v2->transitions, (v2->num_transitions + 1) * sizeof(Transition *));
+    v2->transitions[v2->num_transitions++] = reverse_transition;
+}
+
+void draw_vertices(cairo_t *cr, Vertex *v, double current_angle, double angle_step) {
+    gdk_cairo_set_source_rgba(cr, &v->color);
+    cairo_move_to(cr, v->x, v->y);
+    cairo_arc(cr, v->x, v->y, v->size, current_angle, angle_step);
+    cairo_line_to(cr, v->x, v->y);
+    cairo_fill(cr);
+}
+
+static gboolean draw_callback(GtkWidget *widget, cairo_t *cr, gpointer data) {
+    Vertex **vertices = (Vertex **)data;
+
+    int *row = (int *)malloc(sizeof(int) * 80);
+
+    for (int i = 0; i < 80; i++) {
+        row[i] = 0;
+    }
+    
+    for (int i = 0; i < 69; i++) {
+        Vertex *v = vertices[i];
+        int num_vertices = v->num_transitions;
+        
+        if (num_vertices > 0 && row[v->index] != 1) {
+            double angle_step = 2 * G_PI / (num_vertices+1);
+            double current_angle = 1.5 * G_PI;
+            int n = 1; 
+            if (num_vertices == 2){
+                n = -6;
+            }
+
+            for (int j = 0; j < num_vertices; j++) {
+                Transition *t = v->transitions[j];
+                Vertex *v1 = t->to;
+                Vertex *v2 = t->from;
+                if (j > 0) {
+                    draw_vertices(cr, v1, current_angle, current_angle + angle_step);
+                    cairo_move_to(cr, v->x + n*15, v->y + 4 * (j+4));
+                    cairo_show_text(cr, v1->name);
+                    row[v1->index] = 1;
+                } else {
+                    draw_vertices(cr, v1, current_angle, current_angle + angle_step);
+                    cairo_move_to(cr, v->x + n*15, v->y + 4 * (j-2));
+                    cairo_show_text(cr, v1->name);
+                    row[v1->index] = 1;
+                    current_angle += angle_step;
+                    draw_vertices(cr, v2, current_angle, current_angle + angle_step);
+                    cairo_move_to(cr, v->x + n*15, v->y + 4 * (j+3));
+                    cairo_show_text(cr, v2->name);
+                    row[v2->index] = 1;
+                }
+                current_angle += angle_step;
+            }
+        } else if (row[v->index] != 1) {
+            draw_vertices(cr, v, 0, 2 * G_PI);
+            row[v->index] = 1;
+            cairo_move_to(cr, v->x + 15, v->y + 5);
+            cairo_show_text(cr, v->name);
+        }
+
+        for (int j = 0; j < v->num_edges; j++) {
+            Edge *e = v->edges[j];
+            Vertex *adj_to = e->to;
+            gdk_cairo_set_source_rgba(cr, &v->color);
+            cairo_move_to(cr, v->x, v->y);
+            cairo_line_to(cr, adj_to->x, adj_to->y);
+            cairo_stroke(cr);
+        }
+    }
+    return TRUE;
+}
+
+static int is_transition(Vertex *v1, Vertex *v2) {
+    for (int i = 0; i < v1->num_transitions; i++) {
+        Transition *t = v1->transitions[i];
+        if (t->to == v2) {
+            return true;
+        } 
+    }
+    return false;
+}
+
+static gboolean on_mouse_press(GtkWidget *widget, GdkEventButton *event, gpointer data) {
+    if (event->button == GDK_BUTTON_PRIMARY) {
+        Vertex **vertices = (Vertex **)data;
+
+        // Находим все вершины в радиусе клика
+        for (int i = 0; i < 69; i++) {
+            Vertex *v = vertices[i];
+            double distance = sqrt(pow(v->x - event->x, 2) + pow(v->y - event->y, 2));
+            if (distance < 10) {
+                if (selected_vertex_start == NULL) {
+                    selected_vertex_start = v;
+                    selected_vertex_start->size = 15; // Увеличиваем размер выбранной вершины
+                    break;
+                }else if(v->size == 15) {
+                    continue;
+                } else {
+                    if (is_transition(selected_vertex_start, v)) {
+                        //selected_vertex_start->size = 10; 
+                        selected_vertex_start = v;
+                        selected_vertex_start->size = 15; // Увеличиваем размер выбранной вершины
+                    } else {
+                        selected_vertex_end = v;
+                        selected_vertex_end->size = 15; 
+                    }
+                    break;
+                }
+            }
+        }
+        gtk_widget_queue_draw(widget);  // Перерисовка виджета
+        if(selected_vertex_start != NULL && selected_vertex_end != NULL) {
+            find_path(selected_vertex_start, selected_vertex_end);
+            printf("Start: %s, End: %s\n", selected_vertex_start->name, selected_vertex_end->name);
+            for (int i=0; i<selected_vertex_start->num_transitions; i++) {
+                Transition *t = selected_vertex_start->transitions[i];
+                t->to->size = 10;
+                t->from->size = 10;
+            }
+            selected_vertex_start = NULL;
+            selected_vertex_end = NULL;
+        }
+    }
+    return true;
+}
+
+void find_path(Vertex *start, Vertex *end) {
+    GList *path = NULL, *min_path = NULL;
+    int min_time = MAX_INT;
+    int *visited = (int *)calloc(69, sizeof(int));
+
+    findFastestPath(start, end, 0, &min_time, visited, &path, &min_path);
+    free(visited);
+
+    // Вывод результата в GUI
+    if (min_time != MAX_INT) {
+        GString *path_str = g_string_new("");
+        for (GList *l = min_path; l != NULL; l = l->next) {
+            Vertex *v = l->data;
+            if (l->next != NULL)
+                g_string_append_printf(path_str, "%s -> ", v->name);
+            else
+                g_string_append_printf(path_str, "%s", v->name);
+        }
+
+        GtkWidget *dialog = gtk_message_dialog_new(NULL, GTK_DIALOG_DESTROY_WITH_PARENT,
+                                                   GTK_MESSAGE_INFO, GTK_BUTTONS_OK,
+                                                   "Путь от %s до %s: %s \nВремя пути: %d мин.", 
+                                                   start->name, end->name, path_str->str, min_time);
+        gtk_dialog_run(GTK_DIALOG(dialog));
+        gtk_widget_destroy(dialog);
+    } else {
+        GtkWidget *dialog = gtk_message_dialog_new(NULL, GTK_DIALOG_DESTROY_WITH_PARENT,
+                                                   GTK_MESSAGE_INFO, GTK_BUTTONS_OK,
+                                                   "Путь от %s до %s не найден.",
+                                                   start->name, end->name);
+        gtk_dialog_run(GTK_DIALOG(dialog));
+        gtk_widget_destroy(dialog);
+    }
+    start->size = 10;
+    end->size = 10;
+    g_list_free(min_path);
+    g_list_free(path);
+}
+
+int main(int argc, char **argv) {
+    // Создание массива вершин
+    Vertex *vertices[69]; // Указатель на массив вершин
+
+    // Определение цветов
+    GdkRGBA colors[] = {
+        {0.8, 0.0, 0.0, 1.0},  // Красный
+        {0.0, 0.0, 1.0, 1.0},  // Синий
+        {0.0, 0.8, 0.0, 1.0},  // Зелёный
+        {0.9, 0.5, 0.0, 1.0},  // Оранжевый
+        {0.5, 0.0, 0.5, 1.0}   // Фиолетовый
+    };
+
+    sqlite3 *db;
+    char *err_msg = 0;
+    sqlite3_stmt *res;
+
+    int rc = sqlite3_open("db.db", &db);
+    
+    if (rc != SQLITE_OK) {
+        
+        fprintf(stderr, "Cannot open database: %s\n", sqlite3_errmsg(db));
+        sqlite3_close(db);
+        
+        return 1;
+    }
+    
+    char *sql = "SELECT * FROM Station;";
+
+    rc = sqlite3_prepare_v2(db, sql, -1, &res, 0);    
+    
+    if (rc != SQLITE_OK) {
+        
+        fprintf(stderr, "Failed to fetch data: %s\n", sqlite3_errmsg(db));
+        sqlite3_close(db);
+        
+        return 1;
+    }    
+
+    while ((rc = sqlite3_step(res)) == SQLITE_ROW) {
+        int id = sqlite3_column_int(res, 0);
+        const unsigned  char *name = sqlite3_column_text(res, 1);
+        int time_on = sqlite3_column_int(res, 2);
+        int time_exit = sqlite3_column_int(res, 3);
+        int x = sqlite3_column_int(res, 5);
+        int y = sqlite3_column_int(res, 6);
+        int color_link = sqlite3_column_int(res, 4);
+        if (color_link >= 1 && color_link <= 5) {
+            vertices[id-1] = create_vertex(id, g_strdup_printf("%s", name), colors[color_link - 1], x, y, time_on, time_exit);
+        }
+        
+        //printf("ID: %d, Name: %s\n", id, name);
+    }
+
+    if (rc != SQLITE_DONE) {
+        fprintf(stderr, "Error in SQL execution: %s\n", sqlite3_errmsg(db));
+    }
+    
+    sql = "SELECT * FROM Times;";
+
+    rc = sqlite3_prepare_v2(db, sql, -1, &res, 0);    
+    
+    if (rc != SQLITE_OK) {
+        
+        fprintf(stderr, "Failed to fetch data: %s\n", sqlite3_errmsg(db));
+        sqlite3_close(db);
+        
+        return 1;
+    }    
+
+    while ((rc = sqlite3_step(res)) == SQLITE_ROW) {
+        int from_station_id = sqlite3_column_int(res, 0);
+        int to_station_id = sqlite3_column_int(res, 1);
+        int time = sqlite3_column_int(res, 2);
+        int peah_time = sqlite3_column_int(res, 3);
+        add_edge(vertices[from_station_id-1], vertices[to_station_id-1], time);
+    }
+
+    if (rc != SQLITE_DONE) {
+        fprintf(stderr, "Error in SQL execution: %s\n", sqlite3_errmsg(db));
+    }
+
+    sql = "SELECT * FROM Transfers;";
+
+    rc = sqlite3_prepare_v2(db, sql, -1, &res, 0);    
+    
+    if (rc != SQLITE_OK) {
+        
+        fprintf(stderr, "Failed to fetch data: %s\n", sqlite3_errmsg(db));
+        sqlite3_close(db);
+        
+        return 1;
+    }    
+
+    while ((rc = sqlite3_step(res)) == SQLITE_ROW) {
+        int from_station_id = sqlite3_column_int(res, 0);
+        int to_station_id = sqlite3_column_int(res, 1);
+        int time = sqlite3_column_int(res, 2);
+        add_transition(vertices[from_station_id-1], vertices[to_station_id-1], time);
+    }
+
+    if (rc != SQLITE_DONE) {
+        fprintf(stderr, "Error in SQL execution: %s\n", sqlite3_errmsg(db));
+    }
+
+    sqlite3_finalize(res);
+    sqlite3_close(db);
+    gtk_init(&argc, &argv);
+
+    // Создание окна и виджетов
+    GtkWidget *window = gtk_window_new(GTK_WINDOW_TOPLEVEL);
+    GtkWidget *drawing_area = gtk_drawing_area_new();
+
+    //gtk_window_set_position(GTK_WINDOW(window), GTK_WIN_POS_CENTER);
+    gtk_window_set_title(GTK_WINDOW(window), "Graph Viewer");
+    gtk_window_set_default_size(GTK_WINDOW(window), 800, 700);
+    g_signal_connect(window, "destroy", G_CALLBACK(gtk_main_quit), NULL);
+
+    gtk_container_add(GTK_CONTAINER(window), drawing_area);
+    g_signal_connect(G_OBJECT(drawing_area), "draw", G_CALLBACK(draw_callback), vertices);
+    g_signal_connect(G_OBJECT(drawing_area), "button-press-event", G_CALLBACK(on_mouse_press), vertices);
+
+    gtk_widget_set_events(drawing_area, gtk_widget_get_events(drawing_area) | GDK_BUTTON_PRESS_MASK);
+
+    gtk_window_set_position(GTK_WINDOW(window), GTK_WIN_POS_CENTER);
+
+
+    gtk_widget_show_all(window);
+    gtk_main();
+
+    return 0;
+}
